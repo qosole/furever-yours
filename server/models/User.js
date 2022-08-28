b9951c65894cf4f8c9847ef045ed74feca6c17fe
Ale i <allleizq@gmail.com>
2022-08-27 22:19:00 -0700

    added user model and added it to index.js

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+//requires mongoose for user model/schema
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function (email) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+                },
+                message: `Please enter a valid email address`
+            }
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    },
+);
+
+
+//initializes the user model
+const User = model('User', userSchema);
+
+module.exports = User;

eb243cf0298aa735952b478144efe75f1b3d1166
ericrothmuller <eric@skymarketingco.com>
2022-08-30 13:53:49 -0700

    saving progress on oauth token and pulling from main

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,6 @@ import './Style.css';
 // Pass users array to the List component as a prop
 export default function App() {
 
-const petFinderKey = "WXh8Fm1P6GydlDOm9kP564MYwUs6ITqhBWye7MEwqcwvyU9XQy";
-const petFinderSecret = "VSBGhUAftbzFDnPwh2pRc3x41qEVKGbMM96U74Uu";
-
-const [accessToken, setAccessToken] = useState(null);
-
-MyToken(setAccessToken);
-
   return (
     <div>
       <Header />
--- client/src/components/oauth-token.js
+++ client/src/components/oauth-token.js
@@ -1,32 +1,38 @@
-import { useEffect, createContext, useState } from "react";
+// import { useEffect, useContext, useState } from "react";
+
+// const AuthContext = React.createContext();
+// const petFinderKey = "WXh8Fm1P6GydlDOm9kP564MYwUs6ITqhBWye7MEwqcwvyU9XQy";
+// const petFinderSecret = "VSBGhUAftbzFDnPwh2pRc3x41qEVKGbMM96U74Uu";
+
+// function MyToken ({ Component, pageProps }) {
+//     const [accessToken, setAccessToken] = useState(null);
+
+// useEffect(() => {
+//     const fetchAccessToken = async () => {
+//         const params = new URLSearchParams();
+//         params.append("grant_type", "client_credentials");
+//         params.append("client_id", petFinderKey);
+//         params.append("client_secret", petFinderSecret);
+//         const petfinderRes = await fetch(
+//             "https://api.petfinder.com/v2/oauth2/token",
+//             {
+//                 method: "POST",
+//                 body: params
+//             }
+//         );
+//         setAccessToken(await petfinderRes.json());
+//     };
+//     fetchAccessToken();
+//     console.log(accessToken);
+// }, []);
+
+// return (
+//     <AuthContext.Provider>
+
+//     </AuthContext.Provider>
+// );
+// }
 
 
-const petFinderKey = "WXh8Fm1P6GydlDOm9kP564MYwUs6ITqhBWye7MEwqcwvyU9XQy";
-const petFinderSecret = "VSBGhUAftbzFDnPwh2pRc3x41qEVKGbMM96U74Uu";
 
-const MyToken = async (setAccessToken) => {
-    // const [accessToken, setAccessToken] = useState(null);
-
-
-    const fetchAccessToken = async () => {
-        const params = new URLSearchParams();
-        params.append("grant_type", "client_credentials");
-        params.append("client_id", petFinderKey);
-        params.append("client_secret", petFinderSecret);
-        const petfinderRes = await fetch(
-            "https://api.petfinder.com/v2/oauth2/token",
-            {
-                method: "POST",
-                body: params
-            }
-        );
-        setAccessToken(await petfinderRes.json());
-    };
-    await fetchAccessToken();
-
-// console.log(accessToken);
-
-return
-}
-
-export default MyToken;
+// export default MyToken;

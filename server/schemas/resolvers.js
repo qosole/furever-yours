37b25b2d429a691305f69efc2d1d7f172eceee6b
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-30 22:49:22 -0400

    remote user resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,30 +23,38 @@ const resolvers = {
 
     Mutation: {
         addUser: async (parent, { name, email, password }) => {
-          const user = await User.create({ name, email, password });
-          const token = signToken(user);
-    
-          return { token, user };
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+
+            return { token, user };
         },
         login: async (parent, { email, password }) => {
-          const user = await User.findOne({ email });
-    
-          if (!user) {
-            throw new AuthenticationError('No profile with this email found!');
-          }
-    
-          const correctPw = await user.isCorrectPassword(password);
-    
-          if (!correctPw) {
-            throw new AuthenticationError('Incorrect password!');
-          }
-    
-          const token = signToken(user);
-          return { token, user };
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        removeUser: async (parent, args, context) => {
+            if (context.user) {
+                //this user also has the same issue as 16 - don't forget user._id
+                return User.findOneAndDelete({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
         },
     }
-    
+
 };
 
 
-    module.exports = resolvers;
+module.exports = resolvers;

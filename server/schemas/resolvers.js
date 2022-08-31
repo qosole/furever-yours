efdbb7631ff0520653a623d3d74756971daa90f0
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-30 22:47:53 -0400

    mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,14 +20,33 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
     },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+          const user = await User.create({ name, email, password });
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No profile with this email found!');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        },
+    }
+    
 };
-
-
-
-
-
-
-
 
 
     module.exports = resolvers;

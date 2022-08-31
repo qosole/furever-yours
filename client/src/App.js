d92546cde272316adef3b99286ffe7efa5d81768
ericrothmuller <eric@skymarketingco.com>
2022-08-31 11:00:47 -0700

    Deleted petfinder api and am moving to adopt-a-pet-api

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,14 @@
-import {useEffect, createContext, useState} from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './Style.css';
-// import { ArgumentOutOfRangeError } from 'rxjs';
 
-export const AuthContext = createContext();
-
-function App({Component, pageProps}) {
-  const [accessToken, setAccessToken] = useState(null);
-
-useEffect(() => {
-  const fetchAccessToken = async () => {
-    const res = await fetch("/api/oauth-token");
-    const json = await res.json();
-    setAccessToken(json.access_token);
-    console.log(await res.json());
-  };
-  fetchAccessToken();
-}, []);
-
-
+// Pass users array to the List component as a prop
+export default function App() {
   return (
-    <AuthContext.Provider value={accessToken}>
+    <div>
       <Header />
       <Footer />
-      <Component {...pageProps} />
-    </AuthContext.Provider>
+    </div>
   );
 }
-
-export default App;

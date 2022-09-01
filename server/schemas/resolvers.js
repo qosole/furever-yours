4e076a19eb3b50b55628a303c7cb6517ad773d78
Mona Lisa <stefanibusiness@gmail.com>
2022-08-31 17:50:17 -0700

    got the user get routes functional

--- server/schemas/resolvers.js
+++
@@ -1,9 +0,0 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
-
-const resolvers = { 
-    // 'code is required here
-};
-
-module.exports = resolvers;

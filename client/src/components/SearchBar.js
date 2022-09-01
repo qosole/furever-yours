750c487725f792ca2d28f6d99697e7d0ee627a82
ericrothmuller <eric@skymarketingco.com>
2022-08-31 21:09:43 -0700

    Got the fetch request to adopt-a-pet to return all pets and render info to the page

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -4,7 +4,7 @@ import useFetch from './fetchRequest';
 import '../Style.css';
 
 function CommitSearch() {
-    const { data, loading, error } = useFetch("https://api.adoptapet.me/search?q=dogs");
+    const { data, loading, error } = useFetch("https://api.adoptapet.me/ap");
 
     if (loading) return <h1> LOADING...</h1>;
 
@@ -12,12 +12,18 @@ function CommitSearch() {
 
     return (
         <div>
-            <h1>
-                test
-                {console.log(data)}
-                {data?.name}
-                {data?.sex}
-            </h1>
+            {console.log(data)}
+            <img src={data?.page[0].pic_url} />
+            <p>Species: {data?.page[0].species_breed.species_name}</p>
+            <p>Breed: {data?.page[0].species_breed.breed_name}</p>
+            <p>Name: {data?.page[0].name}</p>
+            <p>Age: {data?.page[0].age}</p>
+            <p>Sex: {data?.page[0].sex}</p>
+            <p>Color: {data?.page[0].color}</p>
+            <p>Location: {data?.page[0].center.name}</p>
+            <p>City: {data?.page[0].center.city}</p>
+            <p>State: {data?.page[0].center.state}</p>
+            <p>Description: {data?.page[0].desc}</p>
         </div>
     )
 }

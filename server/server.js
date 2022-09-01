009226e1d099390859a90038d9419a80a12f9d85
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-31 22:50:08 -0400

    error

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(require('./routes'));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 
   db.once("open", () => {

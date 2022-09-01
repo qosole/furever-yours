2ba67c9976c0aef2e94409ded899e9a38409956c
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-31 22:46:01 -0400

    add created route to index

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -7,17 +7,21 @@ const {
     createUser,
     updateUser,
     deleteUser, 
+    register, 
+    login
     
 } = require('../../controllers/userController');
 
 //this is combining the get and post route for user
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(getUsers).post(createUser)
+router.route('/register').post(register);
 
 
 router
 .route('/:userId')
 .get(getSingleUser)
 .put(updateUser)
-.delete(deleteUser);
+.delete(deleteUser)
+
 
 module.exports = router;

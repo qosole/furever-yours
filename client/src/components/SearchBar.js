920e3e753a6d64fa4f60e6cfbc0de1228938c384
ericrothmuller <eric@skymarketingco.com>
2022-09-02 10:40:02 -0700

    Initial card divs, class names, and stylings applied

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -11,31 +11,35 @@ function CommitSearch() {
     if (error) console.log(error);
 
     return (
-        <div>
+        <div className="animalSearchResults">
             {/* {console.log(data)} */}
-            <img src={data?.page[0].pic_url} />
-            <p>Species: {data?.page[0].species_breed.species_name}</p>
-            <p>Breed: {data?.page[0].species_breed.breed_name}</p>
-            <p>Name: {data?.page[0].name}</p>
-            <p>Age: {data?.page[0].age}</p>
-            <p>Sex: {data?.page[0].sex}</p>
-            <p>Color: {data?.page[0].color}</p>
-            <p>Location: {data?.page[0].center.name}</p>
-            <p>City: {data?.page[0].center.city}</p>
-            <p>State: {data?.page[0].center.state}</p>
-            {/* <p>Description: {data?.page[0].desc}</p> */}
-            <hr/>
-            <img src={data?.page[1].pic_url} />
-            <p>Species: {data?.page[1].species_breed.species_name}</p>
-            <p>Breed: {data?.page[1].species_breed.breed_name}</p>
-            <p>Name: {data?.page[1].name}</p>
-            <p>Age: {data?.page[1].age}</p>
-            <p>Sex: {data?.page[1].sex}</p>
-            <p>Color: {data?.page[1].color}</p>
-            <p>Location: {data?.page[1].center.name}</p>
-            <p>City: {data?.page[1].center.city}</p>
-            <p>State: {data?.page[1].center.state}</p>
-            {/* <p>Description: {data?.page[1].desc}</p> */}
+            <div className="animalCard">
+                <img className="animalImage" src={data?.page[0].pic_url} />
+                <p>Species: {data?.page[0].species_breed.species_name}</p>
+                <p>Breed: {data?.page[0].species_breed.breed_name}</p>
+                <p>Name: {data?.page[0].name}</p>
+                <p>Age: {data?.page[0].age}</p>
+                <p>Sex: {data?.page[0].sex}</p>
+                <p>Color: {data?.page[0].color}</p>
+                <p>Location: {data?.page[0].center.name}</p>
+                <p>City: {data?.page[0].center.city}</p>
+                <p>State: {data?.page[0].center.state}</p>
+            <p>Description: {data?.page[0].desc}</p>
+            </div>
+
+            <div className="animalCard">
+                <img className="animalImage" src={data?.page[1].pic_url} />
+                <p>Species: {data?.page[1].species_breed.species_name}</p>
+                <p>Breed: {data?.page[1].species_breed.breed_name}</p>
+                <p>Name: {data?.page[1].name}</p>
+                <p>Age: {data?.page[1].age}</p>
+                <p>Sex: {data?.page[1].sex}</p>
+                <p>Color: {data?.page[1].color}</p>
+                <p>Location: {data?.page[1].center.name}</p>
+                <p>City: {data?.page[1].center.city}</p>
+                <p>State: {data?.page[1].center.state}</p>
+                <p>Description: {data?.page[1].desc}</p>
+            </div>
         </div>
     )
 }

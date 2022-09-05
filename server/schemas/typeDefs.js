622d35bf2e74efca86db013cff437f08a1b6f43a
qosole <williamj99wei@gmail.com>
2022-09-05 11:38:52 -0500

    Add apollo models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,31 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-// model required
+    type User {
+        _id: ID
+        username: String
+        email: String
+        pets: [Pet]!
+        password: String
+        location: String
+    }
+
+    type Pet {
+        petId: ID
+        apiId: Number
+        species: String
+        breed: String
+        name: String
+        age: String
+        sex: String
+        color: String
+        location: String
+        city: String
+        state: String
+        pros: String
+        cons: String
+        addedAt: Date
+    }
 
 `;
 

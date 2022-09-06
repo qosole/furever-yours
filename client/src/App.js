ef048a6900dfe6c8075fee977f0c5524c12747fa
qosole <williamj99wei@gmail.com>
2022-09-05 21:37:51 -0500

    Equip React app with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './Style.css';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 // Pass users array to the List component as a prop
 export default function App() {
   return (
+    <ApolloProvider client={client}>
     <div>
       <Header />
       <Footer />
     </div>
+    </ApolloProvider>
   );
+ 
 }

ffc4458686dd15fb8b68daf271115e2202e61c4f
Ale i <allleizq@gmail.com>
2022-09-06 21:45:55 -0700

    create save usestate, add useEffect, and display favorite pet data

--- client/src/components/pages/Account.js
+++ client/src/components/pages/Account.js
@@ -9,6 +9,7 @@ import Login from './login2';
 
 export default function Account() {
   const [user, setUser] = useState(null)
+  const [save, setSave] = useState(null)
 
 
   
@@ -20,22 +21,63 @@ export default function Account() {
     }
   }, [auth])
 
+  useEffect(() => {
+    const save = JSON.parse(localStorage.getItem('savedPets'));
+    if (save) {
+     setSave(save);
+     
+    }
+    console.log(save)
 
-  return (
+  }, []);
+  
+
+//   function displayPet() {
+    
+//     const savedPet = JSON.parse(localStorage.getItem("savedPets"));
+//     console.log(savedPet) 
+// }
+
+
+return (
     auth.loggedIn() ? (
-      <div className="account">
-        <h1>Account</h1>
-          <h3>
+      <div >
+        <h1>Account Information</h1>
+          <div className="account-container">
+          <p>
             {
               user ? `Username: ${user.username}` : 'Home'
             }
-          </h3>
+          </p>
 
-          <h3>
+          <p>
             {
               user ? `Email: ${user.email}` : 'Home'
             }
-          </h3>
+          </p>
+          </div>
+          <div className="pet-box">
+            <h2>Saved pets: </h2>
+            <div className="savedAnimals">
+            {save?.map((animal, i) => {
+                return (<div className="animalCard" key={i}>
+                    <img className="animalImage" src={animal.pic_url} alt="animal"/>
+                    <p>Species: {animal.species_breed.species_name}</p>
+                    <p>Breed: {animal.species_breed.breed_name}</p>
+                    <p>Name: {animal.name}</p>
+                    <p>Age: {animal.age}</p>
+                    <p>Sex: {animal.sex}</p>
+                    <p>Color: {animal.color}</p>
+                    <p>Location: {animal.center.name}</p>
+                    <p>City: {animal.center.city}</p>
+                    <p>State: {animal.center.state}</p>
+                </div>)
+            })
+
+            }     
+            </div>
+            
+          </div>
       </div>
       
 
